2533bbf98cd36a48f28ad75525b08efb439f9a63
slavitnas <slavitnas@gmail.com>
2018-04-15 21:07:32 +0300

    Refactoring the code

--- YoloDetector/Models/YoloModel.cs
+++ YoloDetector/Models/YoloModel.cs
@@ -1,61 +1,176 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using Caliburn.Micro;
 using OpenCvSharp;
+using OpenCvSharp.Dnn;
 
 namespace YoloDetector.Models
 {
     public class YoloModel
     {
         public string Name { get; set; }
-        public string Cfg { get; set; }
-        public string Model { get; set; }
-        public string[] Labels { get; set; }
-        public Scalar[] Colors { get; set; }
+        private string _cfg;
+        private string[] _labels;
+        private Scalar[] _colors;
+        private Net _net;
 
         public static BindableCollection<YoloModel> GetYoloModels()
         {
-            var coco = File.ReadAllLines("yolo/coco/coco.names");
-            var voc = File.ReadAllLines("yolo/voc/voc.names");
-            var cocoColors = Enumerable.Repeat(false, coco.Length).Select(x => Scalar.RandomColor()).ToArray();
-            var vocColors = Enumerable.Repeat(false, voc.Length).Select(x => Scalar.RandomColor()).ToArray();
+            const string cocoPath = "yolo/coco/coco";
+            const string vocPath = "yolo/voc/voc";
+            const string tiny = "tiny";
+
+            var cocoNames = File.ReadAllLines($"{cocoPath}.names");
+            var vocNames = File.ReadAllLines($"{vocPath}.names");
+            var cocoColors = Enumerable.Repeat(false, cocoNames.Length).Select(x => Scalar.RandomColor()).ToArray();
+            var vocColors = Enumerable.Repeat(false, vocNames.Length).Select(x => Scalar.RandomColor()).ToArray();
 
             var yoloModels = new BindableCollection<YoloModel>
             {
                 new YoloModel
                 {
                     Name = "Coco",
-                    Cfg = "yolo/coco/coco.cfg",
-                    Model = "yolo/coco/coco.weights",
-                    Labels = coco,
-                    Colors = cocoColors,
+                    _cfg = $"{cocoPath}.cfg",
+                    _labels = cocoNames,
+                    _colors = cocoColors,
+                    _net = CvDnn.ReadNetFromDarknet($"{cocoPath}.cfg", $"{cocoPath}.weights")
                 },
                 new YoloModel
                 {
                     Name = "Tiny Coco",
-                    Cfg = "yolo/coco/tiny-coco.cfg",
-                    Model = "yolo/coco/tiny-coco.weights",
-                    Labels = coco,
-                    Colors = cocoColors,
+                    _cfg = $"{cocoPath}{tiny}.cfg",
+                    _labels = cocoNames,
+                    _colors = cocoColors,
+                    _net = CvDnn.ReadNetFromDarknet($"{cocoPath}{tiny}.cfg", $"{cocoPath}{tiny}.weights")
                 },
                 new YoloModel
                 {
                     Name = "Voc",
-                    Cfg = "yolo/voc/voc.cfg",
-                    Model = "yolo/voc/voc.weights",
-                    Labels = voc,
-                    Colors = vocColors,
+                    _cfg = $"{vocPath}.cfg",
+                    _labels = vocNames,
+                    _colors = vocColors,
+                    _net = CvDnn.ReadNetFromDarknet($"{vocPath}.cfg", $"{vocPath}.weights")
                 },
                 new YoloModel
                 {
                     Name = "Tiny Voc",
-                    Cfg = "yolo/voc/tiny-voc.cfg",
-                    Model = "yolo/voc/tiny-voc.weights",
-                    Labels = voc,
-                    Colors = vocColors,
+                    _cfg = $"{vocPath}{tiny}.cfg",
+                    _labels = vocNames,
+                    _colors = vocColors,
+                    _net = CvDnn.ReadNetFromDarknet($"{vocPath}{tiny}.cfg", $"{vocPath}{tiny}.weights")
                 }
             };
             return yoloModels;
+        }
+
+        /// <summary>
+        /// Read size from cfg file
+        /// </summary>
+        /// <returns>size of image</returns>
+        private int GetSize()
+        {
+            return int.Parse(
+                new string(File.ReadAllLines(_cfg).First(x => x.StartsWith("width"))
+                    .Where(char.IsDigit).ToArray())
+            );
+        }
+
+        /// <summary>
+        /// Find object from darknet yolo
+        /// </summary>
+        /// <param name="image">input image</param>
+        /// <param name="threshold">minimum threshold</param>
+        /// <param name="text">result percents in string format</param>
+        /// <returns>Image with labels</returns>
+        public Mat FindObjects(Mat image, double threshold, out string text)
+        {
+            // setting blob, parameter are important
+            Mat blob = CvDnn.BlobFromImage(image, 1 / 255.0, new Size(GetSize(), GetSize()),
+                new Scalar(), true, false);
+
+            _net.SetInput(blob, "data");
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            // forward model
+            Mat prob = _net.Forward();
+
+            sw.Stop();
+
+            var result = new StringBuilder();
+            result.AppendLine($"Runtime: {sw.ElapsedMilliseconds} ms");
+
+            /* YOLO2 VOC output
+             0 1 : center                    2 3 : w/h
+             4 : confidence                  5 ~24 : class probability */
+            const int prefix = 5; //skip 0~4
+
+            for (int i = 0; i < prob.Rows; i++)
+            {
+                var confidence = prob.At<float>(i, 4);
+
+                if (confidence > threshold)
+                {
+                    // get classes probability
+                    Cv2.MinMaxLoc(prob.Row[i].ColRange(prefix, prob.Cols), out _, out Point max);
+                    int classes = max.X;
+                    var probability = prob.At<float>(i, classes + prefix);
+
+                    // more accuracy
+                    if (probability > threshold)
+                    {
+                        // get center and width/height
+
+                        var centerX = prob.At<float>(i, 0) * image.Width;
+                        var centerY = prob.At<float>(i, 1) * image.Height;
+                        var width = prob.At<float>(i, 2) * image.Width;
+                        var height = prob.At<float>(i, 3) * image.Height;
+
+                        // label formatting
+                        var label = $"{_labels[classes]} {probability * 100:0.00}%";
+                        result.AppendLine($"confidence {confidence * 100:0.00}% {label}");
+
+                        // avoid left side over edge
+                        var x1 = (centerX - width / 2) < 0 ? 0 : centerX - width / 2;
+
+                        // draw result
+                        image.Rectangle(
+                            new Point(x1, centerY - height / 2),
+                            new Point(centerX + width / 2, centerY + height / 2), _colors[classes],
+                            2
+                        );
+                        var textSize = Cv2.GetTextSize(label, HersheyFonts.HersheyTriplex, 0.5, 1, out var baseline);
+
+                        Cv2.Rectangle(
+                            image,
+                            new Rect(
+                                new Point(x1, centerY - height / 2 - textSize.Height - baseline),
+                                new Size(textSize.Width, textSize.Height + baseline)
+                            ),
+                            _colors[classes],
+                            Cv2.FILLED
+                        );
+
+                        Cv2.PutText(
+                            image,
+                            label,
+                            new Point(x1, centerY - height / 2 - baseline),
+                            HersheyFonts.Italic,
+                            0.5,
+                            Scalar.Black
+                        );
+                    }
+
+                }
+
+            }
+
+            text = result.ToString();
+            return image;
         }
     }
 }
--- YoloDetector/ViewModels/ShellViewModel.cs
+++ YoloDetector/ViewModels/ShellViewModel.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Caliburn.Micro;
 using Microsoft.Win32;
 using OpenCvSharp;
-using OpenCvSharp.Dnn;
 using OpenCvSharp.Extensions;
 using YoloDetector.Models;
 
@@ -44,6 +39,9 @@ namespace YoloDetector.ViewModels
             }
         }
 
+        /// <summary>
+        /// Image
+        /// </summary>
         public WriteableBitmap Image
         {
             get => _image;
@@ -55,6 +53,9 @@ namespace YoloDetector.ViewModels
             }
         }
 
+        /// <summary>
+        /// Textbox
+        /// </summary>
         public string FilePath
         {
             get => _filePath;
@@ -65,8 +66,14 @@ namespace YoloDetector.ViewModels
             }
         }
 
+        /// <summary>
+        /// ComboBox
+        /// </summary>
         public BindableCollection<YoloModel> YoloModels { get; set; } = YoloModel.GetYoloModels();
 
+        /// <summary>
+        /// Selected item of ComboBox
+        /// </summary>
         public YoloModel SelectedYoloModel
         {
             get => _selectedYoloModel;
@@ -77,6 +84,9 @@ namespace YoloDetector.ViewModels
             }
         }
 
+        /// <summary>
+        /// TextBox
+        /// </summary>
         public string Info
         {
             get => _info;
@@ -118,80 +128,8 @@ namespace YoloDetector.ViewModels
 
         public void FindObjects()
         {
-            var cfg = SelectedYoloModel.Cfg;
-            var size = int.Parse(
-                new string(File.ReadAllLines(cfg).First(x => x.StartsWith("width"))
-                    .Where(char.IsDigit).ToArray())
-            );
-
-            var model = SelectedYoloModel.Model;
-            var labels = SelectedYoloModel.Labels;
-            var colors = SelectedYoloModel.Colors;
-            var threshold = 0.3;
-
-            StringBuilder result = new StringBuilder();
-
-            Finish = Start.Clone();
-            var w = Finish.Width;
-            var h = Finish.Height;
-
-            //setting blob, parameter are important
-            var blob = CvDnn.BlobFromImage(Finish, 1 / 255.0, new OpenCvSharp.Size(size, size),
-                new Scalar(), true, false);
-            var net = CvDnn.ReadNetFromDarknet(cfg, model);
-            net.SetInput(blob, "data");
-
-            var sw = new Stopwatch();
-            sw.Start();
-            //forward model
-            var prob = net.Forward();
-            sw.Stop();
-            result.AppendLine($"Runtime: {sw.ElapsedMilliseconds} ms");
-
-            /* YOLO2 VOC output
-             0 1 : center                    2 3 : w/h
-             4 : confidence                  5 ~24 : class probability */
-            const int prefix = 5; //skip 0~4
-
-            for (int i = 0; i < prob.Rows; i++)
-            {
-                var confidence = prob.At<float>(i, 4);
-                if (confidence > threshold)
-                {
-                    //get classes probability
-                    Cv2.MinMaxLoc(prob.Row[i].ColRange(prefix, prob.Cols), out _, out OpenCvSharp.Point max);
-                    var classes = max.X;
-                    var probability = prob.At<float>(i, classes + prefix);
-
-                    if (probability > threshold) //more accuracy
-                    {
-                        //get center and width/height
-                        var centerX = prob.At<float>(i, 0) * w;
-                        var centerY = prob.At<float>(i, 1) * h;
-                        var width = prob.At<float>(i, 2) * w;
-                        var height = prob.At<float>(i, 3) * h;
-                        //label formating
-                        var label = $"{labels[classes]} {probability * 100:0.00}%";
-                        result.AppendLine($"confidence {confidence * 100:0.00}% {label}");
-                        var x1 = (centerX - width / 2) < 0 ? 0 : centerX - width / 2; //avoid left side over edge
-                        //draw result
-                        Finish.Rectangle(new OpenCvSharp.Point(x1, centerY - height / 2),
-                            new OpenCvSharp.Point(centerX + width / 2, centerY + height / 2), colors[classes],
-                            2);
-                        var textSize = Cv2.GetTextSize(label, HersheyFonts.HersheyTriplex, 0.5, 1, out var baseline);
-                        Cv2.Rectangle(Finish, new OpenCvSharp.Rect(
-                                new OpenCvSharp.Point(x1, centerY - height / 2 - textSize.Height - baseline),
-                                new OpenCvSharp.Size(textSize.Width, textSize.Height + baseline)),
-                            colors[classes],
-                            Cv2.FILLED);
-                        Cv2.PutText(Finish, label, new OpenCvSharp.Point(x1, centerY - height / 2 - baseline),
-                            HersheyFonts.Italic, 0.5, Scalar.Black);
-
-                    }
-                }
-            }
-            Finish = Finish;
-            Info = result.ToString();
+            Finish = SelectedYoloModel.FindObjects(Start.Clone(), 0.3, out var infoText);
+            Info = infoText;
         }
     }
 }
